672f91119039ac558923bbb8917538919089a958
Michael C. Herrera <michael.c.herrera@icloud.com>
2026-04-24 17:17:15 -0500

    Implement PostHog

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import Image from "next/image";
+import posthog from "posthog-js";
 
 const ExploreBtn = () => {
+    const handleClick = () => {
+        console.log("CLICK");
+        posthog.capture("explore_events_clicked");
+    };
+
     return (
-        <button id="explore-btn" type="button" className="mx-auto mt-7" onClick={() => console.log("CLICK")}>
-            <a href="#events">Explore Events<Image src="/icons/arrow-down.svg" alt="arror-down" width={24} height={24} /></a>
+        <button id="explore-btn" type="button" className="mx-auto mt-7" onClick={handleClick}>
+            <a href="#events">
+                Explore Events
+                <Image src="/icons/arrow-down.svg" alt="arror-down" width={24} height={24} />
+            </a>
         </button>
     );
 };
